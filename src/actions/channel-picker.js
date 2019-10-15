c3099020f44c9fdca7aee7dfa24b8b3718eaf4d1
Markus Ort <markus@gameplayuniver.se>
2019-10-15 11:05:37 +0200

    Getting Drivers info (not sure which to use)

--- src/actions/channel-picker.js
+++ src/actions/channel-picker.js
@@ -1,4 +1,5 @@
 import { defaultLogger } from "../shared/Logger";
+import { drivers } from "../data/drivers";
 
 export const channelPicker = _ => {
     const checkExists = _ => {
@@ -44,9 +45,67 @@ export const channelPicker = _ => {
         searchContainer().remove();
     });
 
-    searchBar().addEventListener("input", _ => {
-        console.log(searchBar().value);
+    searchBar().addEventListener("keydown", e => {
+        switch (e.key) {
+            case "Enter":
+                submitForm();
+            case "Escape":
+                searchBar().blur();
+                break;
+        }
     });
+
+    searchBar().addEventListener("input", _ => {
+        searchResults();
+    });
+
+    const submitForm = _ => {
+        defaultLogger.info(`Submitted: ${searchBar().value}`);
+    };
+
+    const searchResults = _ => {
+        const input = searchBar().value;
+        const availabilities = document.querySelectorAll(
+            "._2c4vB > .content-wrapper > .content-wrapper__inner > :last-child > :last-child [role=listitem] > button"
+        );
+        let possibilities = [];
+        availabilities.forEach(elem => {
+            switch (elem.textContent) {
+                case "Replay":
+                case "Pit Lane":
+                case "Tracker":
+                case "Data":
+                    possibilities.push(elem.textContent);
+                    return;
+            }
+        });
+    };
+
+    const getData = _ => {
+        const driverBtns = document.querySelectorAll(
+            "._2c4vB > .content-wrapper > .content-wrapper__inner > :last-child > :last-child > :last-child [role=listitem] button"
+        );
+        driverBtns.forEach(elem => {
+            let number = elem.querySelector("span:first-child").textContent;
+            let shorthand = elem.querySelector("span:last-child").textContent;
+            getDriverInfo(shorthand, number, elem);
+        });
+    };
+
+    const getDriverInfo = (name, number, elem) => {
+        let url = `https://f1tv.formula1.com/api/driver/?driver_tla=${name}&driver_racingnumber=${number}&limit=1&fields=first_name,last_name`;
+        let request = new XMLHttpRequest();
+        request.open("GET", url);
+        request.responseType = "json";
+        request.send();
+        request.onload = _ => {
+            let item = request.response.objects[0];
+            elem.setAttribute("firstname", item.first_name);
+            elem.setAttribute("lastname", item.last_name);
+        };
+    };
+
+    getData();
 };
 
 export const ChannelPickerisFocus = _ => {
