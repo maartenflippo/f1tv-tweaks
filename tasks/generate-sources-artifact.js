5ca19f9b1e345621584dc5a4da6fb6b6d59ab33f
Maarten Flippo <maartenflippo@outlook.com>
2020-09-08 12:00:58 +0200

    Fix build task

--- tasks/generate-sources-artifact.js
+++ tasks/generate-sources-artifact.js
@@ -9,6 +9,12 @@ const path = require("path");
 const archiver = require("archiver");
 const info = require("../package.json");
 
+const outputDir = path.resolve(__dirname, "../web-ext-artifacts");
+
+if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+}
+
 const outputPath = path.resolve(
     __dirname,
     `../web-ext-artifacts/f1tv_tweaks-${info.version}-src.zip`
@@ -17,11 +23,11 @@ const outputPath = path.resolve(
 const output = fs.createWriteStream(outputPath);
 const archive = archiver("zip");
 
-output.on("close", function() {
+output.on("close", function () {
     console.log("Built sources artifact.");
 });
 
-archive.on("warning", function(err) {
+archive.on("warning", function (err) {
     if (err.code === "ENOENT") {
         console.error(err);
     } else {
@@ -29,7 +35,7 @@ archive.on("warning", function(err) {
     }
 });
 
-archive.on("error", function(err) {
+archive.on("error", function (err) {
     throw err;
 });
 
@@ -40,22 +46,22 @@ archive.directory(path.resolve(__dirname, "../tasks"), "tasks");
 archive.directory(path.resolve(__dirname, "../public/icons"), "public/icons");
 
 archive.file(path.resolve(__dirname, "../public/manifest.json"), {
-    name: "manifest.json"
+    name: "manifest.json",
 });
 archive.file(path.resolve(__dirname, "../public/layout.css"), {
-    name: "public/layout.css"
+    name: "public/layout.css",
 });
 archive.file(path.resolve(__dirname, "../rollup.config.js"), {
-    name: "rollup.config.js"
+    name: "rollup.config.js",
 });
 archive.file(path.resolve(__dirname, "../package.json"), {
-    name: "package.json"
+    name: "package.json",
 });
 archive.file(path.resolve(__dirname, "../package-lock.json"), {
-    name: "package-lock.json"
+    name: "package-lock.json",
 });
 archive.file(path.resolve(__dirname, "../README.md"), {
-    name: "README.md"
+    name: "README.md",
 });
 
 archive.finalize();
